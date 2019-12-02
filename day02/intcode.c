2530c71a5f962f799f6abb2e7e0e074dbcd33740
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2019-12-02 19:04:18 +0200

    Day 2 part 2 done.

--- day02/intcode.c
+++ day02/intcode.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include"intcode.h"
 #include"../utils/filereader.h"
 
@@ -13,17 +14,47 @@ int main(int argc, char **argv){
 	int len = count_fields(argv[1], ',') - 1;
 	int *code = malloc(len * sizeof(int));
 	read_csv_ints(argv[1], len, code, ',');
+	int *editable_code = malloc(len * sizeof(int));
+	memcpy(editable_code, code, len * sizeof(int));
 
-	code[1] = 12;
-	code[2] = 2;
+	// 1202 case
+	editable_code[1] = 12;
+	editable_code[2] = 2;
+	run_code(editable_code);
+	printf("First value for 1202: %d\n", editable_code[0]);
 
+	// Find 19690720
+	for(int noun=0; noun<=99; noun++){
+		for(int verb=0; verb<=99; verb++){
+			memcpy(editable_code, code, len * sizeof(int));
+			editable_code[1] = noun;
+			editable_code[2] = verb;
+			run_code(editable_code);
+
+			if (editable_code[0] == 19690720){
+				printf("100 * noun + verb: %d\n", 100 * noun + verb);
+				free(code);
+				free(editable_code);
+				exit(EXIT_SUCCESS);
+			}
+		}
+	}
+
+	printf("Suitable verb and noun not found.\n");
+	free(code);
+	free(editable_code);
+	exit(EXIT_FAILURE);
+}
+
+
+/*
+ * Run the intcode in the given code array, editing it in place.
+ */
+void run_code(int* code){
 	int index = 0;
 	while(code[index] != 99){
 		index = handle_operation(code, index);
 	}
-	
-	printf("First value: %d\n", code[0]);
-	exit(EXIT_SUCCESS);
 }
 
 int handle_operation(int* code, int index){
--- day02/intcode.h
+++ day02/intcode.h
@@ -1,1 +1,2 @@
+void run_code(int* code);
 int handle_operation(int* code, int index);
